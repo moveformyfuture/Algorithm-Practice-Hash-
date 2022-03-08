edd93352705cf9356cd2de78791034f0dc322d31
Changjun Park <96476118+moveformyfuture@users.noreply.github.com>
2022-03-08 23:27:24 +0900

    Add files via upload

--- 20220308 전화번호 목록.py
+++ 20220308 전화번호 목록.py
@@ -5,6 +5,7 @@ def solution(phoneBook):
                                                  # zip 하면 앞글자에 맞추어 정렬이 됨
                                                  # 반환값이 list가 아니기 때문에 for문에 대입하는게 아니라면 list 처리 해줘야함
                                                  # index가 0부터 시작하지 않으므로 모든 경우의 수를 반복함
-        if p2.startswith(p1): # p2가 p1로 시작하면 True, 그렇지 않으면 False 반환
+        if p2.startswith(p1): # startswith : p2가 p1로 시작하면 True, 그렇지 않으면 False 반환
+                              # 두 매개변수 중 뒤에것과 앞에것의 일치 여부를 비교
             return False
     return True
