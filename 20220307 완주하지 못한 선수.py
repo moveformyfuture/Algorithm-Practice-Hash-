369249eebf43b2c1137097e81b3ec5f46e0b520a
Changjun Park <96476118+moveformyfuture@users.noreply.github.com>
2022-04-19 23:43:53 +0900

    Add files via upload

--- 20220307 완주하지 못한 선수.py
+++ 20220307 완주하지 못한 선수.py
@@ -1,5 +1,6 @@
-import collections # python이 제공하는 하나의 class
+import collections # Counter 함수를 쓰기 위해 호출
 
 def solution(participant, completion):
-    answer = collections.Counter(participant) - collections.Counter(completion) # Counter : list를 가지고 Counter를 생성하면, Counter는 Key가 이름이고, Value가 count인 dic를 반환
-    return list(answer.keys())[0] # 남아있는 key의 이름값을 반환
+    answer = collections.Counter(participant)-collections.Counter(completion) # Counter() : dic처럼 key와 value로 만들어줌
+                                                                              # Counter() 를 사용해야 객체끼리 빼기 연산 가능
+    return list(answer.keys())[0] # keys()함수 사용해 key만 호출, list화해서 남아있는 key 반환
--- 20220309 위장.py
+++ 20220309 위장.py
@@ -1,3 +1,6 @@
 def solution(clothes):
-    answer = 0
+    from collections import Counter # list의 더하기 빼기
+    from functools import reduce #
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
     return answer
